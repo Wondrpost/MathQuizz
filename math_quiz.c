a8ae1a4d1a3144a02d67dfc4b96f26461b2d8e5f
Jerome-88 <jeromebudianto@gmail.com>
2024-12-31 00:58:04 +0700

    Update math_quiz.c

--- math_quiz.c
+++ math_quiz.c
@@ -105,6 +105,8 @@ void plus(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==2){
@@ -126,6 +128,8 @@ void plus(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==3){
@@ -147,6 +151,8 @@ void plus(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
 }
@@ -174,6 +180,8 @@ void mines(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==2){
@@ -195,6 +203,8 @@ void mines(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==3){
@@ -216,6 +226,8 @@ void mines(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
 }
@@ -243,6 +255,8 @@ void multiply(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==2){
@@ -264,6 +278,8 @@ void multiply(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==3){
@@ -285,24 +301,26 @@ void multiply(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
 }
 void divide(){
     system("cls");
     int level,point=0,lives=3,response,num1,num2;
-    float answer,hasil;
-    printf("welcome to plus quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
+    int answer,hasil;
+    printf("welcome to divide quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
     scanf("%d",&level);
     if(level==1){
         while(lives!=0){
             system("cls");
-            num1=rand() % 11;
-            num2=rand() % 11;
-            printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
+            num1=(rand() % 10) +1;
+            num2=(rand() % 10)+1;
+            printf("\npoin: %d\t\tlives: %d\t\tBulatkan ke bawah!!\n\n",point,lives);
             printf("%d  /  %d =  ",num1,num2);
-            scanf("%.2f", &answer);
-            hasil=(float)num1/num2;
+            scanf("%d", &answer);
+            hasil=num1/num2;
             if(answer==hasil){
                 point+=1;
             }else{
@@ -314,17 +332,19 @@ void divide(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==2){
         while(lives!=0){
             system("cls");
-            num1=rand() % 101;
-            num2=rand() % 101;
-            printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
+            num1=(rand() % 100)+1;
+            num2=(rand() % 100)+1;
+            printf("\npoin: %d\t\tlives: %d\t\tBulatkan ke bawah!!\n\n",point,lives);
             printf("%d  /  %d =  ",num1,num2);
             scanf("%d", &answer);
-            hasil=(float)num1/num2;
+            hasil=num1/num2;
             if(answer==hasil){
                 point+=2;
             }else{
@@ -336,17 +356,19 @@ void divide(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
     if(level==3){
         while(lives!=0){
             system("cls");
-            num1=rand() % 1001;
-            num2=rand() % 1001;
-            printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
+            num1=(rand() % 1000)+1;
+            num2=(rand() % 1000)+1;
+            printf("\npoin: %d\t\tlives: %d\t\tBulatkan ke bawah!!\n\n",point,lives);
             printf("%d  /  %d =  ",num1,num2);
             scanf("%d", &answer);
-            hasil=(float)num1/num2;
+            hasil=num1/num2;
             if(answer==hasil){
                 point+=5;
             }else{
@@ -358,249 +380,8 @@ void divide(){
         scanf("%d",&response);
         if(response==1){
             menu();
+        }else{
+            printf("\\(^-^)/ bye...");
         }
     }
 }
-// #include <stdio.h>
-// #include <stdlib.h>
-// void plus();
-// void menu();
-// void mines();
-// void multiply();
-// void divide();
-// int main() {
-//     menu();
-//     return 0;
-// }
-
-// void menu(){
-//     int a;
-//     printf("welcome to math quiz\nselect operator:\n1.plus\n2.mines\n3.multiply\n4.divided\n");
-//     scanf("%d",&a);
-//     if(a==1){
-//         plus();
-//     }
-//     else if(a==2){
-//         mines();
-//     }
-//     else if(a==3){
-//         multiply();
-//     }
-//     else if(a==4){
-//          divide();
-//     }
-// }
-
-
-// void plus(){
-//     int level,num1=0,num2=0,point=0,lives=3,answer,response;
-//     printf("\nwelcome to plus quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
-//     scanf("%d",&level);
-//     if(level==1){
-//         while(lives!=0){
-//             num1=rand() % 11;
-//             num2=rand() % 11;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  +  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1+num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==2){
-//         while(lives!=0){
-//             num1=rand() % 101;
-//             num2=rand() % 101;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  +  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1+num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==3){
-//         while(lives!=0){
-//             num1=rand() % 1001;
-//             num2=rand() % 1001;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  +  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1+num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-// }
-// void mines(){
-//     int level,num1,num2,point=0,lives=3,answer,response;
-//     printf("welcome to mines quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
-//     scanf("%d",&level);
-//     if(level==1){
-//         while(lives!=0){
-//             num1=rand() % 11;
-//             num2=rand() % 11;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  -  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1-num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==2){
-//         while(lives!=0){
-//             num1=rand() % 101;
-//             num2=rand() % 101;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  -  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1-num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==3){
-//         while(lives!=0){
-//             num1=rand() % 1001;
-//             num2=rand() % 1001;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  -  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1-num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-// }
-// void multiply(){
-//     int level,num1,num2,point=0,lives=3,answer,response;
-//     printf("welcome to multiply quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
-//     scanf("%d",&level);
-//     if(level==1){
-//         while(lives!=0){
-//             num1=rand() % 11;
-//             num2=rand() % 11;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  x  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1*num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==2){
-//         while(lives!=0){
-//             num1=rand() % 101;
-//             num2=rand() % 101;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  x  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1*num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-//     if(level==3){
-//         while(lives!=0){
-//             num1=rand() % 1001;
-//             num2=rand() % 1001;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  x  %d =  ",num1,num2);
-//             scanf("%d", &answer);
-//             if(answer==num1*num2){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-// }
-// void divide(){
-//     int level,point=0,lives=3,response,num1,num2;
-//     float answer,hasil;
-//     printf("welcome to plus quiz\npilih level:\n1.easy\n2.medium\n3.hard\n");
-//     scanf("%d",&level);
-//     if(level==1){
-//         while(lives!=0){
-//             num1=rand() % 100;
-//             num2=rand() % 100;
-//             printf("\npoin: %d\t\tlives: %d\n\n",point,lives);
-//             printf("%d  /  %d =  ",num1,num2);
-//             scanf("%.2f", &answer);
-//             hasil=(float)num1/num2;
-//             if(answer==hasil){
-//                 point+=1;
-//             }else{
-//                 lives-=1;
-//             }
-//         }
-//         printf("game over\nexit(0)\tback to menu(1): ");
-//         scanf("%d",&response);
-//         if(response==1){
-//             menu();
-//         }
-//     }
-// }
